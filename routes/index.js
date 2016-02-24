e1244431cd7a2a1a433395af7ae5f881f7a1bd48
sromanyuk <blacksun@mail.ru>
2016-02-24 10:20:24 +0200

    lobby page restricted for logged in  users

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
+var checkAuth = require('../middleware/checkAuth');
+
 module.exports = function(app) {
     app.get('/', require('./HomePageRoute').get);
     app.get('/login', require('./LoginPageRoute').get);
     app.post('/login', require('./LoginPageRoute').post);
     app.post('/logout', require('./LogOutRoute').post);
     app.get('/register', require('./RegisterPageRoute').get);
-    app.get('/lobby', require('./LobbyPageRoute').get);
+    app.get('/lobby', checkAuth, require('./LobbyPageRoute').get);
     app.get('/users', require('./UsersRoute').get);
     app.get('/user/:id', require('./UserRoute').get);
 };
